7274fc8a0c3cc111b4ce834c984a5959943d1a51
Demo07_08 <phongnvph23556@fpt.edu.vn>
2023-10-12 22:03:02 +0700

    phong push signin, sigup, home

--- app/src/main/java/com/example/duantotnghiep_md27/Activity/Splash_ScreenActivity.java
+++ app/src/main/java/com/example/duantotnghiep_md27/Activity/Splash_ScreenActivity.java
@@ -12,5 +12,6 @@ public class Splash_ScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash_screen);
+        //sửa lại
     }
 }
