4967958613c6a19fb15e1ec8dcb796cc749eb057
Demo07_08 <phongnvph23556@fpt.edu.vn>
2023-10-13 13:04:32 +0700

    phong push bottom nav

--- app/src/main/java/com/example/duantotnghiep_md27/MainActivity.java
+++ app/src/main/java/com/example/duantotnghiep_md27/MainActivity.java
@@ -2,15 +2,30 @@ package com.example.duantotnghiep_md27;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import com.example.duantotnghiep_md27.Fragment.Home_Fragment;
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
 
 public class MainActivity extends AppCompatActivity {
+    BottomNavigationItemView bottomNavigationItemView;
 
+    Home_Fragment home_fragment =new Home_Fragment();
+
+
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bottomNavigationItemView=findViewById(R.id.bottomnavmenu);
+
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.framehome,home_fragment).commit();
 
     }
 
