03a2d109fd7fc16f61e9fb26c5022e4f85eadd78
huyndq00 <huyndq00@gmail.com>
2023-11-20 13:33:14 +0700

    Huy commit getProductByCategory

--- app/src/main/java/com/example/duantotnghiep_md27/Adapter/CategoryAdapter.java
+++ app/src/main/java/com/example/duantotnghiep_md27/Adapter/CategoryAdapter.java
@@ -1,10 +1,8 @@
 package com.example.duantotnghiep_md27.Adapter;
 
 import android.annotation.SuppressLint;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
-import android.graphics.Color;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,14 +10,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
+
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.bumptech.glide.Glide;
-import com.example.duantotnghiep_md27.Api.Clients.RestClient;
 import com.example.duantotnghiep_md27.Model.Category;
 import com.example.duantotnghiep_md27.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -27,20 +26,28 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
 
     List<Category> categoryList;
     Context context;
-    int selectedPosition = 0;
+    OnCategoryClickListener callback;
 
-    public CategoryAdapter(List<Category> categoryList, Context context) {
-        this.categoryList = categoryList;
-        this.context = context;
+    public CategoryAdapter(OnCategoryClickListener callback) {
+        this.callback = callback;
+        this.categoryList = new ArrayList<>();
     }
 
+    // Setter cho dữ liệu
+    public void setData(List<Category> categoryList) {
+        this.categoryList = categoryList;
+        notifyDataSetChanged();
+    }
 
+    // Interface để xử lý sự kiện click trên danh sách loại sản phẩm
+    public interface OnCategoryClickListener {
+        void onCategoryClick(String category_id);
+    }
 
     @NonNull
     @Override
     public MyViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int i) {
         View itemView;
-
         itemView = LayoutInflater.from(parent.getContext())
                 .inflate(R.layout.item_row_category, parent, false);
 
@@ -50,22 +57,27 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
     @SuppressLint("RecyclerView")
     @Override
     public void onBindViewHolder(@NonNull final MyViewHolder holder, int position) {
-
         final Category category = categoryList.get(position);
+
         holder.name.setText(category.getCategory_name());
+        Glide.with(context)
+                .load(category.getImage_url())
+                .into(holder.imageView);
 
-        if(category.getImage_url()!=null){
-            Glide.with(context)
-                    .load(RestClient.BASE_URL+ category.getImage_url())
-                    .into(holder.imageView);
-        }
-
+        holder.ll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (callback != null){
+                    callback.onCategoryClick(category.getCategory_id());
+                }
+            }
+        });
     }
 
+    // Đếm số lượng phần tử trong danh sách
     @Override
     public int getItemCount() {
         return categoryList.size();
-
     }
 
     public class MyViewHolder extends RecyclerView.ViewHolder {
@@ -81,4 +93,6 @@ public class CategoryAdapter extends RecyclerView.Adapter<CategoryAdapter.MyView
             ll = itemView.findViewById(R.id.category_item_ll);
         }
     }
+
+
 }
